0ccf1d2001cdac16bd9c32ac5f9e97a3b3a82eca
Arisa <aohashi@live.com>
2020-12-26 22:33:21 -0500

    Small UI update
    Location will have their name text appear when player enters the location. Font in use is BitScript.

--- Assets/Scripts/RoomCamMove.cs
+++ Assets/Scripts/RoomCamMove.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomCamMove : MonoBehaviour
 {
     public Vector2 cameraChange;
     public Vector3 playerChange;
     private CameraMovement cam;
+
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text Location_TXT;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +23,32 @@ public class RoomCamMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
-    private void OnTriggerEnter2D (Collider2D other)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Camera switches to the new room!");
 
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             cam.minPosition += cameraChange;
             cam.maxPosition += cameraChange;
             other.transform.position += playerChange;
+            if (needText)
+            {
+                StartCoroutine(locationNameCo());
+            }
         }
+    }
+
+    //using corroutine because we only want it a certain amount of time 
+
+    private IEnumerator locationNameCo()
+    {
+        text.SetActive(true);
+        Location_TXT.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
     }
 }
