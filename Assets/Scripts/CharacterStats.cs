46f66f794c066e7fe5879d61d0c640dd9183c554
Arisa <aohashi@live.com>
2020-12-27 13:19:33 -0500

    health and stamina bar implemented
    Used LERP to make the animation smoother

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterStats : MonoBehaviour
+{
+    private Image content;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    private float currentFill;
+
+    public float MyMaxValue { get; set; }
+
+    private float currentValue;
+
+    public float myCurrentValue 
+    {
+      get 
+        { 
+            return currentValue; 
+        }
+
+      set 
+        {
+            //our current value will never go over max value
+            if (value > MyMaxValue)
+            {
+                currentValue = MyMaxValue;
+            }
+            // our current value will never go negative
+            else if (value < 0 )
+            {
+                currentValue = 0;
+            }
+            else
+            {
+                currentValue = value;
+            }
+
+          
+            currentFill = currentValue / MyMaxValue;
+        }
+    }
+
+
+
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        content = GetComponent<Image>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentFill != content.fillAmount)
+        {
+            content.fillAmount = Mathf.Lerp(content.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+        }
+
+           // content.fillAmount = currentFill;
+    }
+
+    public void Initialize(float maxValue, float currentValue)
+    {
+        MyMaxValue = maxValue;
+        myCurrentValue = currentValue;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterStats health;
+
+    [SerializeField]
+    private CharacterStats stamina;
+
+    [SerializeField]
+    private float maxHealth;
+
+    [SerializeField]
+    private float maxStamina;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health.Initialize(maxHealth, maxHealth);
+        stamina.Initialize(maxStamina, maxStamina);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Should be losing HP");
+            health.myCurrentValue -= 10;
+            stamina.myCurrentValue -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Debug.Log("Should be gaining HP");
+            health.myCurrentValue += 10;
+            stamina.myCurrentValue += 10;
+        }
+    }
+}
