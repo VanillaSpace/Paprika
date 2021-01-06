d410fa6e66ed6c3e5812d61b2f0214a5c54ff6c7
Arisa <aohashi@live.com>
2021-01-05 21:43:10 -0500

    Loot Items UI

--- Assets/Scripts/UI_Interface/LootWindow.cs
+++ Assets/Scripts/UI_Interface/LootWindow.cs
@@ -1,42 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LootWindow : MonoBehaviour
 {
     [SerializeField]
     private LootButtons[] lootButtons;
 
-    //for debuggin only
+    private List<List<Item>> pages = new List<List<Item>>();
+
+    private int pageIndex = 0;
+
+    [SerializeField]
+    private Text pageNumber;
+
+    [SerializeField]
+    private GameObject nextBtn, prevBtn;
+
+     //for debuggin only
     [SerializeField]
     private Item[] items;
 
     void Start()
     {
+        List<Item> tmp = new List<Item>();
+        for (int i = 0; i < items.Length; i++)
+        {
+            tmp.Add(items[i]);
+        }
+
+        CreatePages(tmp);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void CreatePages(List<Item> items)
+    {
+        List<Item> page = new List<Item>();
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            page.Add(items[i]);
+
+            if (page.Count == 4 || i == items.Count -1)
+            {
+                pages.Add(page);
+                page = new List<Item>();
+            }
+        }
+
         AddLoot();
     }
 
-   
-    void Update()
-    {
-        
-    }
 
     private void AddLoot()
     {
-        int itemIndex = Random.Range(0,5);
+        if (pages.Count > 0)
+        {
 
-        //set loot button icon
-        lootButtons[itemIndex].MyIcon.sprite = items[itemIndex].MyIcon;
 
-        lootButtons[itemIndex].MyLoot = items[itemIndex];
+            pageNumber.text = pageIndex + 1 + "/" + pages.Count;
 
-        //make sure the loot buttons is visible
-        lootButtons[itemIndex].gameObject.SetActive(true);
+            prevBtn.SetActive(pageIndex > 0);
+            nextBtn.SetActive(pages.Count > 1 && pageIndex < pages.Count - 1);
 
-        string title = string.Format("<color={0}>{1}</color>", QualityColor.MyColors[items[itemIndex].MyQuality], items[itemIndex].MyTitle);
+            // int itemIndex = Random.Range(0,5);
 
-        //Make sure that the title is correct
-        lootButtons[itemIndex].MyTitle.text = title;
+            for (int i = 0; i < pages[pageIndex].Count; i++)
+            {
+                if (pages[pageIndex][i] != null)
+                {
+                    lootButtons[i].MyIcon.sprite = pages[pageIndex][i].MyIcon;
+
+                    lootButtons[i].MyLoot = pages[pageIndex][i];
+
+                    //make sure the loot buttons is visible
+                    lootButtons[i].gameObject.SetActive(true);
+
+                    string title = string.Format("<color={0}>{1}</color>", QualityColor.MyColors[pages[pageIndex][i].MyQuality], pages[pageIndex][i].MyTitle);
+
+                    //Make sure that the title is correct
+                    lootButtons[i].MyTitle.text = title;
+                }
+
+            }
+        }
+
+    }
+
+    public void ClearButtons()
+    {
+        foreach (LootButtons btn in lootButtons)
+        {
+            btn.gameObject.SetActive(false);
+        }
+    }
+
+    public void NextPage()
+    {
+        if (pageIndex < pages.Count -1)
+        {
+            pageIndex++;
+            ClearButtons();
+            AddLoot();
+        }
+    }
+
+    public void PreviousPage()
+    {
+        if (pageIndex > 0)
+        {
+            pageIndex--;
+            ClearButtons();
+            AddLoot();
+        }
     }
 }
