b22bfc38d153536bd7b44ffb8e3533d89090adad
Arisa <aohashi@live.com>
2021-01-08 11:42:58 -0500

    Crafting UI added

---
+++ Assets/Scripts/Crafting/CraftingMaterial.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CraftingMaterial 
+{
+    [SerializeField]
+    private Item item;
+
+    [SerializeField]
+    private int count;
+
+    public int MyCount { get => count; }
+    public Item MyItem { get => item; }
+}
