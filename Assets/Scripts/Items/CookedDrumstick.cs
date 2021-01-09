78225481eb9046718198f4709769b435c09bea10
Arisa <aohashi@live.com>
2021-01-09 10:37:27 -0500

    Updated stats on raw and cooked food

--- Assets/Scripts/Items/CookedDrumstick.cs
+++ Assets/Scripts/Items/CookedDrumstick.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CookedDrumstick", menuName = "Items/CookedDrumstick", order = 3)]
-public class CookedDrumstick : Item
+public class CookedDrumstick : Item, IUseable
 {
+    [SerializeField]
+    private int recoveredHealth;
+
+    public void Use()
+    {
+        Remove();
+
+        Player.MyInstance.MyHealth.myCurrentValue += recoveredHealth;
+    }
 
     public override string GetDescription()
     {
-        return base.GetDescription() + string.Format("\n<color=#00ff00ff>Cooked Food!</color>");
+        return base.GetDescription() + string.Format("\n<color=#00ff00ff>Cooked Food \nRestores {0} HEALTH</color>", recoveredHealth);
     }
+
+
 }
--- Assets/Scripts/Items/RawDrumstick.cs
+++ Assets/Scripts/Items/RawDrumstick.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "RawDrumstick", menuName = "Items/RawDrumstick", order = 4)]
-public class RawDrumstick : Item
+public class RawDrumstick : Item, IUseable
 {
+    [SerializeField]
+    private int damagedHealth;
+
+    public void Use()
+    {
+        Remove();
+
+        Player.MyInstance.MyHealth.myCurrentValue -= damagedHealth;
+    }
 
     public override string GetDescription()
     {
-        return base.GetDescription() + string.Format("\n<color=#00ff00ff>Raw Chicken meat.</color>");
+        return base.GetDescription() + string.Format("\n<color=#00ff00ff>Don't eat it raw!</color>");
     }
+
+
 }
