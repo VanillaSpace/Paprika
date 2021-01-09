33f75f76f1929ac026e727ad6e696ac108daa1cb
Arisa <aohashi@live.com>
2021-01-09 11:23:41 -0500

    small fixes

--- Assets/Scripts/Crafting/Profession.cs
+++ Assets/Scripts/Crafting/Profession.cs
@@ -175,7 +175,7 @@ public class Profession : MonoBehaviour
 
     public void ChangeAmount(int i)
     {
-        if ((amount + 1) > 0 && amount + i <= maxAmount)
+        if ((amount + i) > 0 && amount + i <= maxAmount)
         {
             MyAmount += i;
         }
